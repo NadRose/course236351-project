76b3b8f416b1f043c3da48740e653611a9ff8627
NadRose <nadav.rl1@gmail.com>
2022-01-05 19:52:56 +0200

    Initial commit

---
+++ stub/build/scripts/protoc-gen-grpc-kotlin-1.2.0-jdk7-generateProto-trampoline.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files\Java\jdk1.8.0_211\jre\bin\java.exe" -jar "C:\Users\nadav\.gradle\caches\modules-2\files-2.1\io.grpc\protoc-gen-grpc-kotlin\1.2.0\c82a225d1263ce1fc304792af10225e96a4f1e5a\protoc-gen-grpc-kotlin-1.2.0-jdk7.jar" %*
