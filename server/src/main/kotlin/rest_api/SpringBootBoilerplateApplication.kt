f390385acac304588c03815007bdf0e7c8230951
NadRose <nadav.rl1@gmail.com>
2022-01-08 12:51:06 +0200

    removed imports

--- server/src/main/kotlin/rest_api/SpringBootBoilerplateApplication.kt
+++ server/src/main/kotlin/rest_api/SpringBootBoilerplateApplication.kt
@@ -2,12 +2,6 @@ package rest_api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import rest_api.repository.model.TimedTransaction
-import rest_api.repository.model.Transfer
-import rest_api.repository.model.UTxO
-import java.util.*
-import kotlin.Comparator
-import kotlin.collections.HashMap
 
 @SpringBootApplication
 class SpringBootBoilerplateApplication
